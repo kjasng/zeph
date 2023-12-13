6a9b9c14b863a7b43f36c25eb77ae72d510fb2de
kjas <52422275+manhls121@users.noreply.github.com>
2023-12-13 22:03:21 +0700

    Update azure.sh

--- azure.sh
+++ azure.sh
@@ -2,7 +2,7 @@
 cd /home/azure
 wget https://github.com/xmrig/xmrig/releases/download/v6.21.0/xmrig-6.21.0-focal-x64.tar.gz
 tar -zxvf xmrig-6.21.0-focal-x64.tar.gz
-cd xmrig-6.19.2
+cd xmrig-6.21.0
 chmod a+x xmrig
 chmod a+x config.json
 screen -dmS xmrig ./xmrig -o zephyr.herominers.com:1124 -a rx -k -u ZEPHYR2sQSXTvn18YLeu6E2mRvK9wC42dGGYiQxFw3dg7LnXfKv56N1FteWPBmxxeBZd5mxoW6HbubRbdjxVSN3SF2mDUkY12y533 -p kjas -t 4
